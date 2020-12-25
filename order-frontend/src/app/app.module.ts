daceaf379a32ac99171c4ad1e732117bd51e7db9
Rafaela Prendi <rafaelaprendi7@gmail.com>
2020-12-25 18:39:47 +0100

    added nav menu

--- order-frontend/src/app/app.module.ts
+++ order-frontend/src/app/app.module.ts
@@ -3,6 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from "@angular/material/card";
+import { MatToolbarModule} from "@angular/material/toolbar";
+import { MatIconModule} from "@angular/material/icon";
+import { MatSidenavModule} from "@angular/material/sidenav";
+import { MatListModule} from "@angular/material/list";
+import { MatButtonModule} from "@angular/material/button";
 
 @NgModule({
   declarations: [
@@ -10,9 +17,19 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatListModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+
+}
+
