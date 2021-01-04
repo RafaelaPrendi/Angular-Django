ba73cd48bdd3d0c328da8583cdbd5d0b30fb9862
Rafaela Prendi <rafaelaprendi7@gmail.com>
2021-01-04 18:11:31 +0100

    new components

--- order-frontend/src/app/app.module.ts
+++ order-frontend/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from  '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -10,10 +11,24 @@ import { MatIconModule} from "@angular/material/icon";
 import { MatSidenavModule} from "@angular/material/sidenav";
 import { MatListModule} from "@angular/material/list";
 import { MatButtonModule} from "@angular/material/button";
+import { UserListComponent } from './user-list/user-list.component';
+import { UserDetailsComponent } from './user-details/user-details.component';
+import { GroupListComponent } from './group-list/group-list.component';
+import { GroupDetailsComponent } from './group-details/group-details.component';
+import { CustomerListComponent } from './customer-list/customer-list.component';
+import { CustomerDetailsComponent } from './customer-details/customer-details.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserListComponent,
+    UserDetailsComponent,
+    GroupListComponent,
+    GroupDetailsComponent,
+    CustomerListComponent,
+    CustomerDetailsComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
@@ -25,11 +40,9 @@ import { MatButtonModule} from "@angular/material/button";
     MatSidenavModule,
     MatListModule,
     MatButtonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule {
-
-}
-
+export class AppModule { }
